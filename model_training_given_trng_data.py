84ef29558a90999a9a9a89c766d6077d364f754b
Ho Wei Chin <hwc.howeichin@gmail.com>
2019-03-20 11:31:50 +0800

    Add files via upload
    new changes: process command-line arguments

--- model_training_given_trng_data.py
+++ model_training_given_trng_data.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 from tifffile import imread
 from csbdeep.utils import axes_dict, plot_some, plot_history
@@ -8,6 +8,7 @@ from csbdeep.utils.tf import limit_gpu_memory
 from csbdeep.io import load_training_data
 from csbdeep.models import Config, IsotropicCARE
 from generate_trng_data import *
+import argparse
 
 def config_model(axes, n_channel_in, n_channel_out, epoch):
     config = Config(axes, n_channel_in, n_channel_out, epoch)
@@ -35,15 +36,22 @@ def train(full_storage_dir, validation_percentage, epoch):
 
 
 if __name__ == '__main__':
-    #change these when needed
-    full_img_dir = 'C:/Users/e0031794/Desktop/animal_pic'
-    target_dir = 'retina'
-    subsample_factor = 10.2
-    storage_name = 'training_set'
 
-    #running data generation pipeline
-    raw_data = set_raw_data_dir(full_img_dir, target_dir)
-    anisotropic_transform = set_anisotropic_transform(subsample_factor)
-    full_trng_data_path = generate_anisotropic_training_data(raw_data, anisotropic_transform, storage_name)
+    parser = argparse.ArgumentParser(description='Generate trng data and train model w deep learning')
+    parser.add_argument('--d', type=str, help='Full path of image folder')
+    parser.add_argument('--t', type=str, help='Target subfolder within image folder')
+    parser.add_argument('--sf', type=float, default=10.2, help='subsample_factor for anisotropic transform')
+    parser.add_argument('--str', type=str, help='storage folder for transformed training data')
 
+    args = parser.parse_args()
+    print('setting raw data directory ...')
+    raw_data = set_raw_data_dir(args.d, args.t)
+    print('setting anisotropic transform ...')
+    anisotropic_transform = set_anisotropic_transform(args.sf)
+    print('generating anisotropic training data...')
+    full_trng_data_path = generate_anisotropic_training_data(raw_data, anisotropic_transform, args.str)
+    print('training network....')
     trng_history = train(full_trng_data_path, 10, 30)
+    print('done!')
+
+
